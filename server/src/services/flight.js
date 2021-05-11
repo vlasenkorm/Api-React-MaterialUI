b8167ed4659fe17a3b1802016205c81a17414b12
Roman Vlasenko <roman.vlasenko@p2h.com>
2021-05-11 18:44:19 +0300

    add date check

--- server/src/services/flight.js
+++ server/src/services/flight.js
@@ -10,20 +10,20 @@ const createFlight = (reqInfo) => {
         sourcePortCode: reqInfo.sourcePortCode,
         destinationPortName: reqInfo.destinationPortName,
         destinationPortCode: reqInfo.destinationPortCode,
-        scheduledArrival: Date.now(),
-        scheduledDeparture: Date.now(),
+        scheduledArrival: reqInfo.scheduledArrival ? reqInfo.scheduledArrival : Date.now(),
+        scheduledDeparture: reqInfo.scheduledDeparture ? reqInfo.scheduledDeparture : Date.now(),
         status: reqInfo.status
   });
   
     return flight.save(flight)	
 }
 
-const listFlight = (req) => {
+const listFlight = () => {
     return Flight.find({})
 }
 
 const findFlightByID = (id) => {
-    return Flight.findById(id)	
+    return Flight.findById(id)
 }
 
 const updateFlight = (id, requestBody) => {
@@ -32,5 +32,6 @@ const updateFlight = (id, requestBody) => {
 
 const destroyFlight = (id) => {
     return Flight.findByIdAndRemove(id, { useFindAndModify: false })
-}   
+}
+
 module.exports =  {createFlight, listFlight, findFlightByID, updateFlight,  destroyFlight}
